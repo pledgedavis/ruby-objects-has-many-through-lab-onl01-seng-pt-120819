89e34e84a46cdd6aa7dbd58af45d069beb14b0bd
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 20:42:24 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,43 +1,127 @@
+# class Doctor
+# 
+# attr_accessor :name
+# 
+# @@all = []
+# def self.all
+#     @@all
+#   end
+# 
+#   attr_accessor :name, :appointments, :patient
+# 
+# def initialize(name)
+#  @appointments = []
+# @name = name
+# 
+# @@all << self
+# # self.class.all << self
+# end
+# 
+# def new_appointment(date, patient)
+#   appointment = Appointment.new(patient, date, self)
+#   @appointments << appointment
+#     appointment
+# end
+# 
+# 
+# def patients
+#     @appointments.collect do |appointment|
+#       appointment.patient
+#     end
+#   end
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Doctor
+  @@all=[]
 
-attr_accessor :name
-
-@@all = []
-def self.all
+  def self.all
     @@all
   end
 
   attr_accessor :name, :appointments, :patient
 
-def initialize(name)
- @appointments = []
-@name = name
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
 
-@@all << self
-# self.class.all << self
-end
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient, date, self)
+     @appointments << appointment
+     appointment
+  end
 
-def new_appointment(date, patient)
-  appointment = Appointment.new(patient, date, self)
-  @appointments << appointment
-    appointment
-end
-
-
-def patients
+  def patients
     @appointments.collect do |appointment|
       appointment.patient
     end
   end
+end
 
 
+class Patient
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name=name
+  end
+  
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
 
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
+  
+end
 
+class Appointment
+  attr_accessor :patient, :doctor, :date
 
+  @@all = []
 
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
 
+    @@all << self
+  end
 
-
-
+  def self.all
+    @@all
+  end
 end
