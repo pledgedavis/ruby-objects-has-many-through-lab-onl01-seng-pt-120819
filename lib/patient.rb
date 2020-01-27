0b7e23388e1ca03ec4190813b9b667e32d72301c
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 20:45:47 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -32,8 +32,12 @@ class Patient
 @@all = []
   def initialize(name)
     @name=name
+    @@all << self
   end
+def self.all
+  @@all
 
+end
    def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
