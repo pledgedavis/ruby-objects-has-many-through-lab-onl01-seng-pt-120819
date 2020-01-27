295c6359643e693e08a3a5779fb137392971c84b
Pledge Davis <pledgedavis14@gmail.com>
2020-01-27 20:44:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,28 +1,53 @@
+# class Patient
+#  attr_accessor :name
+#  def initialize(name)
+#      @name = name
+#  end
+#
+# def new_appointment(doctor, date)
+# # my_new_appointment = Appointment.new(doctor, self, date)
+# Appointment.new(date, self, doctor)
+# end
+#
+# def appointments
+#    Appointment.all.select do |appointment|
+#      appointment.patient == self
+#    end
+#  end
+#
+#  def doctors
+#    appointments.map do |appointment|
+#      appointment.doctor
+#    end
+#  end
+#
+# end
+
+
+
+
+
 class Patient
- attr_accessor :name
- def initialize(name)
-     @name = name
- end
+  attr_accessor :name
 
-def new_appointment(doctor, date)
-# my_new_appointment = Appointment.new(doctor, self, date)
-Appointment.new(date, self, doctor)
-end
+  def initialize(name)
+    @name=name
+  end
 
-def appointments
-   Appointment.all.select do |appointment|
-     appointment.patient == self
-   end
- end
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
 
- def doctors
-   appointments.map do |appointment|
-     appointment.doctor
-   end
- end
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
 
-
-
-
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
 
 end
